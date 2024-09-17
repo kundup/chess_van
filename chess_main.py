c9a69f5e20acc917db898d3cb9fd4ba8767757e0
kundup <kundup61@hotmail.com>
2024-09-16 18:02:18 -0700

    chess images transformed into the size

--- chess_main.py
+++ chess_main.py
@@ -8,10 +8,25 @@ pygame.init()
 screen_width = 640
 screen_height = 640
 game_title = "chess_van"
+size = 60
 
 # display
 screen = pygame.display.set_mode((screen_width, screen_height))
-game_name = pygame.display.set_caption(game_title)
+pygame.display.set_caption(game_title)
+
+# images
+black_bishop = pygame.transform.scale(pygame.image.load("b_bishop.png").convert_alpha(), (size, size))
+black_king = pygame.transform.scale(pygame.image.load("b_king.png").convert_alpha(), (size, size))
+black_knight = pygame.transform.scale(pygame.image.load("b_knight.png").convert_alpha(), (size, size))
+black_pawn = pygame.transform.scale(pygame.image.load("b_pawn.png").convert_alpha(), (size, size))
+black_queen = pygame.transform.scale(pygame.image.load("b_queen.png").convert_alpha(), (size, size))
+black_rook = pygame.transform.scale(pygame.image.load("b_rook.png").convert_alpha(), (size, size))
+white_bishop = pygame.transform.scale(pygame.image.load("w_bishop.png").convert_alpha(), (size, size))
+white_king = pygame.transform.scale(pygame.image.load("w_king.png").convert_alpha(), (size, size))
+white_knight = pygame.transform.scale(pygame.image.load("w_knight.png").convert_alpha(), (size, size))
+white_pawn = pygame.transform.scale(pygame.image.load("w_pawn.png").convert_alpha(), (size, size))
+white_queen = pygame.transform.scale(pygame.image.load("w_queen.png").convert_alpha(), (size, size))
+white_rook = pygame.transform.scale(pygame.image.load("w_rook.png").convert_alpha(), (size, size))
 
 
 # game loop
